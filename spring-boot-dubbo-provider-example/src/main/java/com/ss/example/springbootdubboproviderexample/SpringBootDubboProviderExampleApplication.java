4d9d8f5167efc5aa64093dd5c0457aa55937d1d2
shisongWjj <sslovewjj891029>
2020-08-16 16:22:57 +0800

    first Commit

--- spring-boot-dubbo-provider-example/src/main/java/com/ss/example/springbootdubboproviderexample/SpringBootDubboProviderExampleApplication.java
+++ spring-boot-dubbo-provider-example/src/main/java/com/ss/example/springbootdubboproviderexample/SpringBootDubboProviderExampleApplication.java
@@ -1,8 +1,10 @@
 package com.ss.example.springbootdubboproviderexample;
 
+import org.apache.dubbo.config.spring.context.annotation.DubboComponentScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@DubboComponentScan("com.ss.example.springbootdubboproviderexample")
 @SpringBootApplication
 public class SpringBootDubboProviderExampleApplication {
 
