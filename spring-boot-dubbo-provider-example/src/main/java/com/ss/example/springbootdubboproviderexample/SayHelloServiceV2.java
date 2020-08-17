3c155ee86823376b8cd786c3e249633c5c86ede9
shisongWjj <sslovewjj891029>
2020-08-17 21:35:36 +0800

    first Commit

--- spring-boot-dubbo-provider-example/src/main/java/com/ss/example/springbootdubboproviderexample/SayHelloServiceV2.java
+++ spring-boot-dubbo-provider-example/src/main/java/com/ss/example/springbootdubboproviderexample/SayHelloServiceV2.java
@@ -1,7 +1,9 @@
 package com.ss.example.springbootdubboproviderexample;
 
 import com.ss.example.ISayHelloService;
+import org.apache.dubbo.config.annotation.Argument;
 import org.apache.dubbo.config.annotation.DubboService;
+import org.apache.dubbo.config.annotation.Method;
 
 /**
  * SayHelloService
@@ -9,7 +11,11 @@ import org.apache.dubbo.config.annotation.DubboService;
  * @author shisong
  * @date 2020-08-11
  */
-@DubboService(registry = {"shanghai","hunan"},version = "2.0")
+@DubboService(registry = {"shanghai","hunan"},version = "2.0",methods = {
+        @Method(name = "sayHello",arguments = {
+                @Argument()
+        })
+})
 public class SayHelloServiceV2 implements ISayHelloService {
     @Override
     public String sayHello(String name) {
